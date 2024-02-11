afc346694ff73412e0d82d710483ea48d33ffacf
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-02-11 21:02:43 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    vector<int>temp;
+    for(auto s:operations){
+        if(s[0]=='I'){
+            string i;
+            for(int j=2;j<s.size();j++){
+                i+=s[j];
+            }
+            temp.push_back(stoi(i));
+        }else if(s[0]=='D'&&temp.size()!=0){
+            if(s[2]!='-'){
+                priority_queue<int> pq;
+                for(auto i:temp){
+                    pq.push(i);
+                }
+                temp.erase(find(temp.begin(),temp.end(),pq.top()));
+            }else{
+                priority_queue<int> pq;
+                for(auto i:temp){
+                    pq.push(-i);
+                }
+                temp.erase(find(temp.begin(),temp.end(),-pq.top()));
+            }
+            
+        }
+        
+        
+    }
+    
+    if(temp.size()==0){
+        return {0,0};
+    }else if(temp.size()==1){
+        return {temp[0],temp[0]};    
+    }
+    sort(temp.begin(),temp.end());
+    return {temp[temp.size()-1],temp[0]};
+    
+}
