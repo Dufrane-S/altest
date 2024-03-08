d4c0c5b30705ebea6be6d76cf3e07d4080fa6f2e
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-03-08 15:52:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int dp[1001];
+int main(void){
+    int x;
+    cin>>x;
+    dp[1]=1;
+    dp[2]=2;
+
+    for(int i=3;i<=x;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%10007;
+    }
+    cout<<dp[x];
+}
