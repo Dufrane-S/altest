b8e428ba8de31e91328ff1ce52a9c70b856daee2
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-03-20 12:08:40 +0900

    [Silver III] Title: N과 M (3), Time: 492 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x,y;
+int arr[10];
+
+void func(int n){
+    if(n==y){
+        for(int i=0;i<y;i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    for(int i=1;i<=x;i++){
+        arr[n]=i;
+        func(n+1);
+    }
+
+}
+
+
+int main(void){
+    cin>>x>>y;
+    func(0);
+}
