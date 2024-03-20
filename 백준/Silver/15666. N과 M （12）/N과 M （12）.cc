7509a88b545f2f7ac9a69a6d35505f8c16734163
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-03-20 13:40:54 +0900

    [Silver II] Title: N과 M (12), Time: 8 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x,y;
+int arr[10];
+map<string ,int>m;
+vector<int>nums;
+void func(int n){
+    if(n==y){
+        string s;
+        for(int i=0;i<y;i++){
+            s+=to_string(arr[i]);
+            s+='+';
+        }
+        if(m[s]==0){
+            for(int i=0;i<y;i++){
+                cout<<arr[i]<<' ';
+            }
+            cout<<'\n';
+            m[s]++;
+        }
+        return;
+    }
+    for(int i=0;i<x;i++){
+        if(arr[n-1]>nums[i])continue;
+        arr[n]=nums[i];
+        func(n+1);
+    }
+
+}
+
+
+int main(void){
+    cin>>x>>y;
+    for(int i=0;i<x;i++){
+        int a;
+        cin>>a;
+        nums.push_back(a);
+    }
+    sort(nums.begin(),nums.end());
+    func(0);
+}
