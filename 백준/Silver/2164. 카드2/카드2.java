bce8320634b60eba0ddc9b63fefad27375651415
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-10-28 14:07:21 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 43256 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		LinkedList<Integer> list = new LinkedList();
+		for(int i=1;i<=n;i++) {
+			list.addLast(i);
+		}
+		while(list.size()>1) {
+			list.removeFirst();
+			list.addLast(list.removeFirst());
+		}
+
+		System.out.println(list.get(0));
+	}
+}
