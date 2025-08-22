7f29fb172fffada6417e54b7a3299c47425998f7
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2025-08-22 21:30:19 +0900

    [Silver V] Title: Olympiad Pizza, Time: 112 ms, Memory: 13344 KB -BaekjoonHub

---
+++ 백준/Silver/15235. Olympiad Pizza/Olympiad Pizza.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
+        int[] answers = new int[t];
+        for (int i = 0; i < t; i++) {
+            q.add(Integer.parseInt(st.nextToken()));
+            q2.add(i);
+        }
+
+        int count = 1;
+        while (!q2.isEmpty()) {
+            int now = q.poll();
+            int order = q2.poll();
+            if (--now > 0) {
+                q.add(now);
+                q2.add(order);
+            } else {
+                answers[order] = count;
+            }
+            count++;
+        }
+        for (int now : answers) {
+            System.out.print(now + " ");
+        }
+
+    }
+}
